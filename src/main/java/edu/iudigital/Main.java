531d71de20c6c8c8cf6b0ee931f63ddcf4c8afd6
marlon <marlon.monterrosa.m@gmail.com>
2026-03-07 21:16:57 -0500

    se crea un menu con do while el caso 1 registra l;os vehiculos caso 2  registra entrda caso 3 salir

--- src/main/java/edu/iudigital/Main.java
+++ src/main/java/edu/iudigital/Main.java
@@ -10,5 +10,69 @@ public class Main {
         VehiculoDAO vehiculoDAO = new VehiculoDAO();
         CeldaDAO celdaDAO = new CeldaDAO();
 
+        int opcion;
+
+        do{
+
+            System.out.println("\n=== AUTOS COLOMBIA ===");
+            System.out.println("1 Registrar Vehiculo");
+            System.out.println("2 Registrar Entrada");
+            System.out.println("3 Salir");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch(opcion){
+
+                case 1:
+
+                    System.out.print("Placa: ");
+                    String placa = sc.nextLine();
+
+                    System.out.print("Tipo: ");
+                    String tipo = sc.nextLine();
+
+                    System.out.print("Marca: ");
+                    String marca = sc.nextLine();
+
+                    System.out.print("Color: ");
+                    String color = sc.nextLine();
+
+                    Vehiculo v = new Vehiculo(placa,tipo,marca,color);
+
+                    vehiculoDAO.guardarVehiculo(v);
+
+                    break;
+
+                case 2:
+
+                    System.out.print("Placa: ");
+                    String placaEntrada = sc.nextLine();
+
+                    if(!vehiculoDAO.existeVehiculo(placaEntrada)){
+                        System.out.println("Vehiculo no registrado");
+                        break;
+                    }
+
+                    Integer celda = celdaDAO.buscarCeldaLibre();
+
+                    if(celda==null){
+                        System.out.println("No hay celdas disponibles");
+                    }else{
+
+                        celdaDAO.ocuparCelda(celda);
+
+                        System.out.println("Vehiculo ingresado en celda "+celda);
+
+                    }
+
+                    break;
+
+            }
+
+        }while(opcion!=3);
+
+
+
     }
 }
